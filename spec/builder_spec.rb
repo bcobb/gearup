73c05eb50116a5fa6450c88c4d0436da67d8644f
Brian <bcobb@uwalumni.com>
2012-03-10 12:06:11 -0600

    Add Gearup::Builder.build example

--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -3,9 +3,20 @@ require 'gearup'
 describe Gearup::Builder do
 
   it 'can build workers from files' do
+    payload = stub(:data => 'reverse')
     worker = Gearup::Builder.build_from_file('example/reverse.rb')
 
-    worker.work(stub(:data => 'reverse')).should == 'esrever'
+    worker.work(payload).should == 'esrever'
+  end
+
+  it 'can build workers' do
+    payload = stub(:data => 'echo me!')
+
+    worker = Gearup::Builder.build do
+      enable lambda { |payload| payload.data }
+    end
+
+    worker.work(payload).should == 'echo me!'
   end
 
 end
