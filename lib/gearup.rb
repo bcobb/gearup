25531379a2d7bf171bc6c8a12a3e8774d1808928
Brian <bcobb@uwalumni.com>
2012-03-29 11:37:12 -0500

    Extracting methods on Gearup

--- lib/gearup.rb
+++ lib/gearup.rb
@@ -3,6 +3,90 @@ require 'gearman'
 
 module Gearup
 
+  class << self
+    attr_reader :logger, :configuration
+  end
+
+  def self.run_from_file(file, configuration)
+    @configuration = configuration # XXX: smelly
+    start_logging
+
+    eval ::File.read(file), TOPLEVEL_BINDING, file
+
+    ability = Example::Echo.new
+
+    worker.add_ability('example.echo') do |data, job|
+      payload = OpenStruct.new(:data => data)
+
+      ability.call(payload)
+    end
+
+    worker.after_ability('example.echo') do |result, data|
+      payload = OpenStruct.new(:data => data)
+
+      logger.debug("Got #{result.inspect} from #{payload.inspect}")
+    end
+
+    start
+  end
+
+  def self.start
+    # XXX: option to run in foreground?
+    daemonize
+    remember_to_stop
+
+    loop { worker.work }
+  end
+
+  def self.worker
+    @worker ||= lambda do
+      options = { :network_timeout_sec => 2, :reconnect_sec => 4 }
+
+      worker = Gearman::Worker.new(configuration[:servers], options)
+      def worker.abilities ; @abilities.keys ; end # tsk
+      worker
+    end.call
+  end
+
+  def self.start_logging
+    @logger = Logger.new(configuration[:logfile])
+    @logger.level = configuration[:loglevel]
+    Gearman::Util.logger = @logger
+  end
+
+  def self.daemonize
+    if RUBY_VERSION < "1.9"
+      exit if fork
+      ::Process.setsid
+      exit if fork
+      STDIN.reopen "/dev/null"
+      STDOUT.reopen "/dev/null", "a"
+      STDERR.reopen "/dev/null", "a"
+    else
+      ::Process.daemon
+    end
+
+    # XXX: configuration[:pid]
+    pid_file = ::File.expand_path('gearup.pid')
+    ::File.open(pid_file, 'w'){ |f| f.write("#{::Process.pid}") }
+
+    logger.debug("Wrote out #{pid_file}")
+
+    at_exit { ::File.delete(pid_file) if ::File.exist?(pid_file) }
+  end
+
+  def self.remember_to_stop
+    trap(:INT) do
+      logger.debug "Gearup: Shutting down"
+
+      # XXX: Gearup::Worker#shutdown
+      worker.abilities.each { |ability| worker.remove_ability(ability) }
+      worker.worker_enabled = false
+
+      exit
+    end
+  end
+
   class CommandLineConfiguration
 
     def self.from(args)
@@ -23,7 +107,8 @@ module Gearup
     def options
       {
         :logfile => ::File.expand_path('log/gearup.log'),
-        :servers => ['localhost:4730']
+        :servers => ['localhost:4730'],
+        :loglevel => Logger::INFO
       }.merge(@options)
     end
 
@@ -39,6 +124,7 @@ module Gearup
 
         parser.on('-v', '--verbose', 'Enable verbose (DEBUG-level) logging') do |verbose|
           @options[:verbose] = true
+          @options[:loglevel] = Logger::DEBUG
         end
 
         parser.on('-l', '--logfile LOGFILE', "Specify Gearup's log location") do |logfile|
@@ -49,65 +135,6 @@ module Gearup
       end
     end
 
-  end
-
-  def self.run_from_file(file, configuration)
-    logger = Logger.new(configuration[:logfile])
-    if configuration[:verbose]
-      logger.level = Logger::DEBUG
-    else
-      logger.level = Logger::INFO
-    end
-
-    eval ::File.read(file), TOPLEVEL_BINDING, file
-
-    ability = Example::Echo.new
-
-    Gearman::Util.logger = logger
-    options = { :network_timeout_sec => 2, :reconnect_sec => 4 }
-    worker = Gearman::Worker.new(configuration[:servers], options)
-    worker.add_ability('example.echo') do |data, job|
-      payload = OpenStruct.new(:data => data)
-
-      ability.call(payload)
-    end
-
-    worker.after_ability('example.echo') do |result, data|
-      payload = OpenStruct.new(:data => data)
-
-      logger.debug("Got #{result.inspect} from #{payload.inspect}")
-    end
-
-    logger.debug "Daemonizing"
-
-    if RUBY_VERSION < "1.9"
-      exit if fork
-      Process.setsid
-      exit if fork
-      STDIN.reopen "/dev/null"
-      STDOUT.reopen "/dev/null", "a"
-      STDERR.reopen "/dev/null", "a"
-    else
-      Process.daemon
-    end
-
-    pid_file = File.expand_path('gearup.pid')
-    logger.debug("Writing out #{pid_file}")
-    ::File.open(pid_file, 'w'){ |f| f.write("#{Process.pid}") }
-    at_exit { ::File.delete(pid_file) if ::File.exist?(pid_file) }
-
-    trap(:INT) do
-      logger.debug "Gearup: Shutting down"
-      worker.remove_ability('example.echo')
-
-      worker.worker_enabled = false
-
-      exit
-    end
-
-    logger.debug "Daemonized"
-
-    loop { worker.work }
   end
 
 end
